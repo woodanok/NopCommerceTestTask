dbd9396a054c43b30bf60967a84d64081fed9b7f
Vadim Shevchenko <wdanok@mail.ru>
2022-12-13 12:26:08 +0300

    Add project files.

---
+++ Presentation/Nop.Web.Framework/Models/BaseNopModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Nop.Web.Framework.Models
+{
+    /// <summary>
+    /// Represents base nopCommerce model
+    /// </summary>
+    public partial record BaseNopModel
+    {
+        #region Ctor
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        public BaseNopModel()
+        {
+            PostInitialize();
+        }
+
+        #endregion
+
+        #region Methods
+        
+        /// <summary>
+        /// Perform additional actions for the model initialization
+        /// </summary>
+        /// <remarks>Developers can override this method in custom partial classes in order to add some custom initialization code to constructors</remarks>
+        protected virtual void PostInitialize()
+        {
+        }
+
+        #endregion
+    }
+}
