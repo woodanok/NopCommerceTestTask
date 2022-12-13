c94f512db5d404bd206217f2578708e003166531
Vadim Shevchenko <wdanok@mail.ru>
2022-12-13 12:29:25 +0300

    Nop-1: Added the author property for the product table

--- Libraries/Nop.Core/Domain/Catalog/Product.cs
+++ Libraries/Nop.Core/Domain/Catalog/Product.cs
@@ -612,5 +612,10 @@ namespace Nop.Core.Domain.Catalog
             get => (RentalPricePeriod)RentalPricePeriodId;
             set => RentalPricePeriodId = (int)value;
         }
+
+        /// <summary>
+        /// Gets or sets the author
+        /// </summary>
+        public string Author { get; set; }
     }
 }
---
+++ Libraries/Nop.Data/Migrations/AddAuthorProperty.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+using Nop.Core.Domain.Catalog;
+
+namespace Nop.Data.Migrations
+{
+    [NopMigration("2022/12/13 12:00:00:2551770", "Category. Add author property", UpdateMigrationType.Data, MigrationProcessType.Update)]
+    public class AddAuthorProperty : AutoReversingMigration
+    {
+        /// <summary>Collect the UP migration expressions</summary>
+        public override void Up()
+        {
+            Create.Column(nameof(Product.Author))
+            .OnTable(nameof(Product))
+            .AsString(255)
+            .Nullable();
+        }
+    }
+}
